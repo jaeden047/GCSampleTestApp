f82f9e2ae408437d9fd2cc86adba204795700153
Emily Chang <emily0109.ec@gmail.com>
2025-05-21 12:28:24 -0400

    db set up, install dotenv for security

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
+// routes/users.js
 var express = require('express');
 var router = express.Router();
+const db = require('../db'); // make sure path is correct
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  db.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      return next(err);
+    }
+    res.json(results); // send results as JSON
+  });
 });
 
 module.exports = router;
