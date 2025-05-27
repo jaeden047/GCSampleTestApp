45486b087be356db23b7fec6561043f98382d738
vanilliqs2 <jaeden.george.personal@gmail.com>
2025-05-27 15:35:09 -0400

    Main menu.js & Results.js

--- routes/form.js
+++ routes/form.js
@@ -7,22 +7,57 @@ var router = express.Router();
 
 // Main Menu (Contact Info, Select Form) -> Form (Grade 8-12) -> Results -> Main Menu
 router.post('/', (req, res) => {
-  // Session data, results after form, etc.
-  res.send('Here are your results!');
 });
 
-router.post('/submit', (req, res) => { 
-  console.log("Name: " + req.body.Name);
-  console.log("Email: " + req.body.Email);
-  console.log("Phone: " + req.body.Phone);
-  console.log("Grade: " + req.body.Grade);
-  console.log(req.body);
-  // req.body will have list of hard-coded parameters, hidden/public, such as grade, question number, 
-  // and relevant user answer for question number. This data will be saved to the database.
+import { getGrade } from './mainmenu.js'; // USE FOR DATABASE SAVING, IS IT LOCAL?
 
-  // req.body.answerOne or answerTwo, etc. will be cross checked through if-statements, as it's a multiple choice 
-  // exam. The form then calculates the graded result based on internal logic, and then it saves the results in database.
+router.post('/g8/submit', (req, res) => { 
+  console.log("Question 1: " + req.body.Q1);
+  console.log("Question 2: " + req.body.Q2);
+  console.log("Question 3: " + req.body.Q3);
+  console.log("Question 4: " + req.body.Q4);
+  console.log("Question 5: " + req.body.Q5);
+  console.log("Question 6: " + req.body.Q6);
+  console.log("Question 7: " + req.body.Q7);
+  console.log("Question 8: " + req.body.Q8);
+  console.log("Question 9: " + req.body.Q9);
+  console.log("Question 10: " + req.body.Q10);
+  // SAVE INTO DATABASE USING GRADE**
   
+  res.redirect('/form/results'); // Results form will recollect data from consistent session ID number, username, password, email, all submitted at the start of the form.
+  // The session data will be saved in req.session.username, etc. Results form will then display results pertaining to the parameter using views folder, images, and other tools.
+})
+
+router.post('/g910/submit', (req, res) => { 
+  console.log("Question 1: " + req.body.Q1);
+  console.log("Question 2: " + req.body.Q2);
+  console.log("Question 3: " + req.body.Q3);
+  console.log("Question 4: " + req.body.Q4);
+  console.log("Question 5: " + req.body.Q5);
+  console.log("Question 6: " + req.body.Q6);
+  console.log("Question 7: " + req.body.Q7);
+  console.log("Question 8: " + req.body.Q8);
+  console.log("Question 9: " + req.body.Q9);
+  console.log("Question 10: " + req.body.Q10);
+  // SAVE INTO DATABASE**
+  
+  res.redirect('/form/results'); // Results form will recollect data from consistent session ID number, username, password, email, all submitted at the start of the form.
+  // The session data will be saved in req.session.username, etc. Results form will then display results pertaining to the parameter using views folder, images, and other tools.
+})
+
+router.post('/g1112/submit', (req, res) => { 
+  console.log("Question 1: " + req.body.Q1);
+  console.log("Question 2: " + req.body.Q2);
+  console.log("Question 3: " + req.body.Q3);
+  console.log("Question 4: " + req.body.Q4);
+  console.log("Question 5: " + req.body.Q5);
+  console.log("Question 6: " + req.body.Q6);
+  console.log("Question 7: " + req.body.Q7);
+  console.log("Question 8: " + req.body.Q8);
+  console.log("Question 9: " + req.body.Q9);
+  console.log("Question 10: " + req.body.Q10);
+  // SAVE INTO DATABASE UNDER MAIN MENU LOGIN**
+
   res.redirect('/form/results'); // Results form will recollect data from consistent session ID number, username, password, email, all submitted at the start of the form.
   // The session data will be saved in req.session.username, etc. Results form will then display results pertaining to the parameter using views folder, images, and other tools.
 })
