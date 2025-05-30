efde92766f2cc3c65a0d72d83819649b715f2374
milychang19 <emily0109.ec@gmail.com>
2025-05-30 11:04:05 -0400

    update topic_id

--- testingdata.sql
+++ testingdata.sql
@@ -23,7 +23,7 @@ VALUES
 
 -- Question 2
 INSERT INTO Questions (topic_id, question_text)
-VALUES (1, 'If f(x) = 2x² - 3x + 1, what is f(-2)?');
+VALUES (2, 'If f(x) = 2x² - 3x + 1, what is f(-2)?');
 INSERT INTO Answers (question_id, answer_text, is_correct)
 VALUES 
   (2, '11', TRUE),
@@ -33,7 +33,7 @@ VALUES
 
 -- Question 3
 INSERT INTO Questions (topic_id, question_text)
-VALUES (1, 'Solve: 3(2x - 4) = 2(3x + 1)');
+VALUES (2, 'Solve: 3(2x - 4) = 2(3x + 1)');
 INSERT INTO Answers (question_id, answer_text, is_correct)
 VALUES 
   (3, 'x = 11', TRUE),
@@ -43,7 +43,7 @@ VALUES
 
 -- Question 4
 INSERT INTO Questions (topic_id, question_text)
-VALUES (1, 'Which equation represents a line parallel to y = 3x - 4?');
+VALUES (2, 'Which equation represents a line parallel to y = 3x - 4?');
 INSERT INTO Answers (question_id, answer_text, is_correct)
 VALUES 
   (4, 'y = 3x + 2', TRUE),
@@ -53,7 +53,7 @@ VALUES
 
 -- Question 5
 INSERT INTO Questions (topic_id, question_text)
-VALUES (1, 'Simplify: (2x^3y^2)(3xy^4)');
+VALUES (2, 'Simplify: (2x^3y^2)(3xy^4)');
 INSERT INTO Answers (question_id, answer_text, is_correct)
 VALUES 
   (5, '6x^4y^5', TRUE),
