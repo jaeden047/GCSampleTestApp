c0991c756a724830319a84cf4cfe2e2ba482ee37
milychang19 <emily0109.ec@gmail.com>
2025-06-11 14:09:25 -0400

    organize app.js

--- backend/app.js
+++ backend/app.js
@@ -1,43 +1,45 @@
-// To run from scratch, run node setup.js on terminal after downloading mySQL, readying connection, workbench, etc.
-// Then run npm start to test the backend.
+// run npm start to start the backend
+
+// Import modules
 var createError = require('http-errors');
 var path = require('path');
 var logger = require('morgan');
-
 require('dotenv').config(); // Load env variables
+
+// Express
 const express = require('express');
 const cookieParser = require('cookie-parser');
-
-const indexRouter = require('./routes');
-
 const app = express();
 
+// Router for handling API requests
+const indexRouter = require('./routes');
+
+// Database connection
+require('./db'); // needed to open & access database file
+
+// Server Setup
 app.listen(3000, '0.0.0.0', () => {
   console.log('Server running on http://0.0.0.0:3000');
 });
 
-app.use(express.json()); // for parsing JSON request bodies
-
-require('./db'); // needed to open & access database file
+app.use(express.json()); // for parsing application/json request bodies (req.body)
+app.use(express.urlencoded({ extended: true })); // if form sends data -> put data into req.body (requires encoded url tied to front-end submission)
+app.use(cookieParser()); // read parser 
+app.use(express.static(path.join(__dirname, 'public'))); // serves files from public folder
+app.use(logger('dev')); // prints to the dev console ("morgan")
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views')); // creates a path to the views folder
 app.set('view engine', 'jade'); // sets the view engine to jade so that HTML uses the .jade files
 
-app.use(logger('dev')); // prints to the dev console
-app.use(express.json()); // tells express to receive from the frontend (i.e. req.body = { key: value })
-app.use(express.urlencoded({ extended: true })); // if form sends data -> put data into req.body (requires encoded url tied to front-end submission)
-app.use(cookieParser()); // read parser 
-app.use(express.static(path.join(__dirname, 'public'))); // serves files from public folder
-
+// API route handling
 app.use('/api', indexRouter);
 
-// catch 404 and forward to error handler
+// Error Handling
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404)); // route is not found
 });
 
-// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
