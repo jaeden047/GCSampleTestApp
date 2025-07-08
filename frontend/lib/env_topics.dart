71ee2a9514041bf762c51da87d5190ff0c1c241d
milychang19 <emily0109.ec@gmail.com>
2025-07-08 19:19:33 -0400

    coming soon image

--- frontend/lib/env_topics.dart
+++ frontend/lib/env_topics.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 // EnvTopics page
 // Showcase all environmental quiz topics for the users to select
 // It is not in development stage for now
@@ -11,12 +12,28 @@ class EnvTopics extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Environmental Topics'),
-        
       ),
       body: Center(
-        child: Text(
-          'Coming Soon',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Vertically center the content
+          crossAxisAlignment: CrossAxisAlignment.center, // Horizontally center the content
+          children: [
+            // SVG Image
+            SvgPicture.asset(
+              'assets/images/grey_leaf.svg',
+              height: 100,
+            ),
+            SizedBox(height: 20), // Space between the image and the text
+            // Text message
+            Text(
+              'Coming Soon',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'More quizzes to come',
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
     );
