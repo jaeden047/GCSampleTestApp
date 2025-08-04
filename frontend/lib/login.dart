5e556794216a0e6cca69f557daa1c4ac86a5dfcf
vanilliqs2 <jaeden.george.personal@gmail.com>
2025-08-04 15:24:50 -0400

    Resolved merge: kept login.dart

---
+++ frontend/lib/login.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:supabase_flutter/supabase_flutter.dart'; // supabase flutter sdk
+import 'home.dart';
+
+// Null issue
+// The fact you can back out during a one-attempt quiz -> resolve that
+// "Are you sure?" check - verifying all blanks are filled
+// Profile check
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  final phoneController = TextEditingController();
+  final supabase = Supabase.instance.client;
+
+  bool _isLogin = true; // true = Login, false = Sign Up
+
+  Future<void> submit() async {
+    final email = emailController.text.trim();
+    final password = passwordController.text;
+
+    final emailCheck = emailController.text.trim();
+    final passwordCheck = passwordController.text;
+    final name = nameController.text.trim();
+    final phone = phoneController.text.trim();
+
+    if (_isLogin) {
+      if (emailCheck.isEmpty || passwordCheck.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Please fill in both email and password.')),
+        );
+        return;
+      }
+    } else {
+      if (emailCheck.isEmpty || passwordCheck.isEmpty || name.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Please fill in name, email, and password.')),
+        );
+        return;
+      }
+    }
+
+    try {
+      if (_isLogin) {
+        // Login
+        final response = await supabase.auth.signInWithPassword(
+          email: email,
+          password: password,
+        );
+        if (response.user != null) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Logged in as ${response.user?.email}')),
+          );
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => Home()),
+          );
+        }
+      } else {
+        // Sign Up
+        final phone = phoneController.text.trim();
+        final response = await supabase.auth.signUp(
+          email: email,
+          password: password,
+          // phone: phone,
+          data:{
+            'phone': phone // Saved as metadata, because phone can't interfere with login.
+          }
+        );
+        if (response.user != null) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Sign up successful. Please verify your email.')),
+          );
+          // Insert to profiles table
+          final userId = response.user?.id;
+          await supabase.from('profiles').insert({
+            'id': userId,
+            'name': nameController.text.trim(),
+            'email': email,
+            'phone_number': phone,
+          });
+
+        }
+      }
+    } on AuthException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.message)),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(_isLogin ? 'Login' : 'Sign Up')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/gcFuture.png',
+              height: 140,
+            ),
+            if (_isLogin == false)...[
+              const SizedBox(height: 12),
+              TextField(
+              controller: nameController,
+              decoration: const InputDecoration(labelText: 'Name'),
+              ),
+            ],
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            const SizedBox(height: 12),
+            TextField(
+              controller: passwordController,
+              obscureText: false,
+              decoration: const InputDecoration(labelText: 'Password'),
+            ),
+            if (_isLogin == false)...[
+              const SizedBox(height: 12),
+              TextField(
+              controller: phoneController,
+              obscureText: false,
+              decoration: const InputDecoration(labelText: 'Phone Number (Optional)'),
+              ),
+            ],
+            const SizedBox(height: 20),
+            GestureDetector(
+              onTap: submit,
+              child: SvgPicture.asset(
+                _isLogin ? 'assets/images/login_button.svg' : 'assets/images/signup_button.svg',
+              ),
+            ),
+            const SizedBox(height: 12),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _isLogin = !_isLogin;
+                });
+              },
+              child: Text(_isLogin
+                  ? "Don't have an account? Sign up"
+                  : "Already have an account? Login"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
