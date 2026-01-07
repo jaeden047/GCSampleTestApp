804b137a1f9b5380d4f123990e3f46b9d16a9a17
Jaeden George <jaeden.george.personal@gmail.com>
2026-01-07 16:39:55 -0500

    File Deletion

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Centralize all route files
-router.use('/users', require('./users'));
-router.use('/quiz', require('./quiz'));
-router.use('/results', require('./results'));
-
-module.exports = router;
